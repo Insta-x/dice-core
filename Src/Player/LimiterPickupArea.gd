f13c03e0fbfb6449fc00152ef2aef673fe6da56d
Insta-x <uttsadajason@outlook.com>
2022-07-16 22:06:39 +0700

    Pickup Limiter

--- Src/Player/LimiterPickupArea.gd
+++ Src/Player/LimiterPickupArea.gd
@@ -1,16 +1,12 @@
 extends Area2D
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+export (NodePath) onready var limiter = get_node(limiter) as Limiter
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pick_limiter"):
+		var detected_limiter : Limiter = get_overlapping_areas()[0]
+		limiter.upper_limit = detected_limiter.upper_limit
+		limiter.lower_limit = detected_limiter.lower_limit
+		detected_limiter.queue_free()
