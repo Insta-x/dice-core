f10fe3a0a96971d833f629a01bc2f33b6ee4d1df
Insta-x <uttsadajason@outlook.com>
2022-07-16 14:56:47 +0700

    Test game world

--- Src/Player/Player.gd
+++ Src/Player/Player.gd
@@ -7,16 +7,12 @@ var movespeed := 200
 var velocity := Vector2.ZERO
 
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("shoot"):
-		emit_signal("player_shot")
-	
-	get_tree().set_input_as_handled()
-
-
 func _physics_process(delta: float) -> void:
 	velocity += move_and_slide(get_direction() * movespeed)
 	look_at(get_global_mouse_position())
+	
+	if Input.is_action_just_pressed("shoot"):
+		emit_signal("player_shot")
 
 
 func get_direction() -> Vector2:
