ead9bccdffc162395b72983a3bfa9bbabdb0a993
Insta-x <uttsadajason@outlook.com>
2022-07-16 22:08:59 +0700

    Pickup Limiter

--- Src/Player/LimiterPickupArea.gd
+++ Src/Player/LimiterPickupArea.gd
@@ -6,7 +6,11 @@ export (NodePath) onready var limiter = get_node(limiter) as Limiter
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pick_limiter"):
+		print("test")
+		if get_overlapping_areas().size() == 0:
+			return
 		var detected_limiter : Limiter = get_overlapping_areas()[0]
 		limiter.upper_limit = detected_limiter.upper_limit
 		limiter.lower_limit = detected_limiter.lower_limit
 		detected_limiter.queue_free()
+		get_tree().set_input_as_handled()
