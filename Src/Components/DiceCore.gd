13027ba7d76aea190fe34c7c117a0f1859f6c634
Insta-x <uttsadajason@outlook.com>
2022-07-16 14:43:05 +0700

    GUI

--- Src/Components/DiceCore.gd
+++ Src/Components/DiceCore.gd
@@ -4,6 +4,7 @@ class_name DiceCore
 
 
 export (int) var init_number := 1
+export (Texture) var dice_icon
 export (NodePath) onready var limiter = get_node(limiter) as Limiter
 
 var current_number := init_number
@@ -12,5 +13,4 @@ var current_number := init_number
 func _next() -> int:
 	var new_number = current_number * 3
 	current_number = limiter.limit(new_number)
-	print("next", current_number)
 	return current_number
