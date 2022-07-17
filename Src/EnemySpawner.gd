272ef9b7b2052916f728bd036c7c14c0e288aa67
Insta-x <uttsadajason@outlook.com>
2022-07-17 21:54:37 +0700

    Rebalance spawn enemy

--- Src/EnemySpawner.gd
+++ Src/EnemySpawner.gd
@@ -3,7 +3,9 @@ extends Node2D
 
 const enemy_scenes := [
 	preload("res://Src/Enemy/Ranged/RangedEnemy1.tscn"),
-	preload("res://Src/Enemy/Slime/SlimeEnemy1.tscn"),
+	preload("res://Src/Enemy/Slime/SlimeEnemy1.tscn")
+]
+const tank_scenes := [
 	preload("res://Src/Enemy/Tank/TankEnemy1.tscn"),
 	preload("res://Src/Enemy/Tank/TankEnemy2.tscn"),
 	preload("res://Src/Enemy/Tank/TankEnemy3.tscn"),
@@ -16,22 +18,29 @@ const spawn_anim_sprite := preload("res://Src/Enemy/SpawnAnimation.tscn")
 export (NodePath) onready var player = get_node(player) as Player
 
 func spawn() -> void:
-	var select : int = randi() % enemy_scenes.size()
+	var select : int = randi() % 3
 	var pos : int = randi() % get_children().size()
-	if get_child(pos).global_position.distance_to(player.global_position) < 50:
+	print(player.global_position.distance_squared_to(get_child(pos).global_position))
+	if player.global_position.distance_squared_to(get_child(pos).global_position) < 100000:
 		pos += 1
 	
-	var enemy : Enemy = enemy_scenes[select].instance()
+	var enemy : Enemy
+	if select == 2:
+		var sel := randi() % 5
+		enemy = tank_scenes[sel].instance()
+	else:
+		enemy = enemy_scenes[select].instance()
+	
 	var spawn_anim : AnimatedSprite = spawn_anim_sprite.instance()
 	enemy.spawn(player)
 	enemy.global_position = get_child(pos).global_position
-	spawn_anim.global_position = enemy.global_position
+	spawn_anim.global_position = get_child(pos).global_position
 	get_parent().call_deferred("add_child", enemy)
 	get_parent().call_deferred("add_child", spawn_anim)
 
 
 func _ready() -> void:
 	pass
-	for i in range(6):
-		yield(get_tree().create_timer(0.5), "timeout")
-		spawn()
+#	for i in range(10):
+#		yield(get_tree().create_timer(0.5), "timeout")
+#		spawn()
