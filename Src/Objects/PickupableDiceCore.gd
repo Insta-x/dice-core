7166981d834855b197fe0e51d49aba24e93a62a6
Insta-x <uttsadajason@outlook.com>
2022-07-18 00:31:35 +0700

    Pickupable fix

--- Src/Objects/PickupableDiceCore.gd
+++ Src/Objects/PickupableDiceCore.gd
@@ -4,3 +4,11 @@ class_name PickupableDiceCore
 
 
 export (Resource) var dice_core_resource = dice_core_resource as DiceCoreResource
+
+
+func _ready() -> void:
+	$Sprite.texture = dice_core_resource.small_dice_icon
+	$Sprite.modulate = dice_core_resource.color
+	
+	yield(get_tree().create_timer(10), "timeout")
+	queue_free()
