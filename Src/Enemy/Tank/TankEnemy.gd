5e482f861a9a84b832287eaf48e2f8a27bfe11c2
HelpMePliz <87262213+HelpMePliz@users.noreply.github.com>
2022-07-17 13:14:10 +0700

    tank enemy

---
+++ Src/Enemy/Tank/TankEnemy.gd
@@ -0,0 +1,13 @@
+extends Enemy
+
+func _physics_process(delta:float)->void:
+	match current_roll:
+		0:
+			self_destruct()
+		1:
+			move_to_player()
+		2:
+			move_random()
+		_: 
+			do_nothing()
+	data.init = false
