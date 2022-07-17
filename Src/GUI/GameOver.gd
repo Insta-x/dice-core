eaa910ff94390913919990c35df0c223a0ecd026
Insta-x <uttsadajason@outlook.com>
2022-07-17 16:52:31 +0700

    Proper game over

--- Src/GUI/GameOver.gd
+++ Src/GUI/GameOver.gd
@@ -6,8 +6,10 @@ func _ready() -> void:
 
 
 func _input(event: InputEvent) -> void:
-	if visible and event.is_action_pressed("pause"):
-		get_tree().change_scene("res://Src/Game.tscn")
+	if visible:
+		if event.is_action_pressed("pause"):
+			get_tree().change_scene("res://Src/Game.tscn")
+		get_tree().set_input_as_handled()
 
 
 func _on_player_died() -> void:
--- Src/GUI/PausePanel.gd
+++ Src/GUI/PausePanel.gd
@@ -5,3 +5,5 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		get_tree().paused = not get_tree().paused
 		visible = not visible
+		
+		get_tree().set_input_as_handled()
