c3464d0962e98c64217132aa16b7da5b264bda22
Insta-x <uttsadajason@outlook.com>
2022-07-17 09:56:38 +0700

    Fix limiter

--- Src/Components/Limiter.gd
+++ Src/Components/Limiter.gd
@@ -4,11 +4,21 @@ class_name Limiter
 
 
 # Just change these, no need to extend except special case
-export (int) var upper_limit := 6
-export (int) var lower_limit := 0
+export (int) var upper_limit := 6 setget set_upper_limit
+export (int) var lower_limit := 0 setget set_lower_limit
 
 var modulo := upper_limit - lower_limit + 1
 
 
 func limit(number: int) -> int:
-	return number % (modulo) + lower_limit
+	return number % modulo + lower_limit
+
+
+func set_upper_limit(value: int) -> void:
+	upper_limit = value
+	modulo = upper_limit - lower_limit + 1
+
+
+func set_lower_limit(value: int) -> void:
+	lower_limit = value
+	modulo = upper_limit - lower_limit + 1
