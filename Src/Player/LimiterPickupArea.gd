d69f05d916c1f5c9340a95638f4f4e3b6748734f
Insta-x <uttsadajason@outlook.com>
2022-07-17 09:24:10 +0700

    Test PickupableLimiter

--- Src/Player/LimiterPickupArea.gd
+++ Src/Player/LimiterPickupArea.gd
@@ -9,7 +9,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		if get_overlapping_areas().size() == 0:
 			return
 		
-		var detected_limiter : Limiter = get_overlapping_areas()[0]
+		var detected_limiter : PickupableLimiter = get_overlapping_areas()[0]
 		limiter.upper_limit = detected_limiter.upper_limit
 		limiter.lower_limit = detected_limiter.lower_limit
 		GlobalSignals.emit_signal("player_limiter_changed", limiter.lower_limit, limiter.upper_limit)
