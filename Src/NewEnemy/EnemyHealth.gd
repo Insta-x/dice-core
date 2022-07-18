58fee16099d1db2f48a476e91e63adb6e0645c53
Insta-x <uttsadajason@outlook.com>
2022-07-18 17:09:04 +0700

    Remove BodyPosition

--- Src/NewEnemy/EnemyHealth.gd
+++ Src/NewEnemy/EnemyHealth.gd
@@ -3,7 +3,7 @@ extends Node
 class_name EnemyHealth
 
 
-export (NodePath) onready var body_position = get_node(body_position) as Position2D
+export (NodePath) onready var body = get_node(body) as Enemy
 export (NodePath) onready var dice_wrapper = get_node(dice_wrapper) as DiceWrapper
 
 export (int) var max_health = 3
@@ -11,7 +11,7 @@ export (int) var crit_damage := 3
 export (bool) var immune_to_more = false
 export (bool) var immune_to_less = false
 export (bool) var immune_to_same = false
-export (bool) var immune_to_even = true
+export (bool) var immune_to_even = false
 export (bool) var immune_to_odd = false
 export (bool) var weak_to_same = true
 
@@ -29,23 +29,23 @@ func hurt(roll: int) -> void:
 	var current_roll : int = dice_wrapper.get_number(false)
 	
 	if immune_to_more and roll > current_roll:
-		GlobalSignals.emit_signal("text_popup", "immune to higher", body_position.global_position)
+		GlobalSignals.emit_signal("text_popup", "immune to higher", body.global_position)
 		return
 
 	if immune_to_less and roll < current_roll:
-		GlobalSignals.emit_signal("text_popup", "immune to lower", body_position.global_position)
+		GlobalSignals.emit_signal("text_popup", "immune to lower", body.global_position)
 		return
 
 	if immune_to_same and roll == current_roll:
-		GlobalSignals.emit_signal("text_popup", "immune to equal", body_position.global_position)
+		GlobalSignals.emit_signal("text_popup", "immune to equal", body.global_position)
 		return
 
 	if immune_to_even and roll % 2 == 0:
-		GlobalSignals.emit_signal("text_popup", "immune to even", body_position.global_position)
+		GlobalSignals.emit_signal("text_popup", "immune to even", body.global_position)
 		return
 
 	if immune_to_odd and roll % 2 == 1:
-		GlobalSignals.emit_signal("text_popup", "immune to odd", body_position.global_position)
+		GlobalSignals.emit_signal("text_popup", "immune to odd", body.global_position)
 		return
 	
 	if weak_to_same and roll == current_roll:
--- Src/NewEnemy/EnemyWrapper.gd
+++ Src/NewEnemy/EnemyWrapper.gd
@@ -1,19 +1,21 @@
 extends Node2D
 
+class_name EnemyWrapper
+
 
 const pickup_dice_core_scn := preload("res://Src/Objects/PickupableDiceCore.tscn")
 const pickup_limiter_scn := preload("res://Src/Objects/PickupableLimiter.tscn")
 
+export (NodePath) onready var body = get_node(body) as Enemy
 export (int) var score := 1
 
 onready var enemy_gui := $GUIwrapper
-onready var body_position := $BodyPosition
 onready var dice_behaviour_machine := $DiceBehaviourMachine
 onready var dice_wrapper := $DiceWrapper
 onready var dice_timer := $DiceTimer
 
 
-func spawn(dice_core: DiceCoreResource, lower_limit: int, upper_limit: int) -> void:
+func init(dice_core: DiceCoreResource, lower_limit: int, upper_limit: int) -> void:
 	dice_wrapper.set_new_dice_core(dice_core)
 	dice_wrapper.set_new_limit(lower_limit, upper_limit)
 
@@ -28,13 +30,13 @@ func dead() -> void:
 	
 	var drop_dice : PickupableDiceCore = pickup_dice_core_scn.instance()
 	drop_dice.dice_core_resource = $DiceWrapper/DiceCore.dice_core_resource
-	drop_dice.global_position = body_position.global_position - Vector2(40, 0)
+	drop_dice.global_position = body.global_position - Vector2(40, 0)
 	get_parent().call_deferred("add_child", drop_dice)
 	
 	var drop_limiter : PickupableLimiter = pickup_limiter_scn.instance()
 	drop_limiter.lower_limit = $DiceWrapper/Limiter.lower_limit
 	drop_limiter.upper_limit = $DiceWrapper/Limiter.upper_limit
-	drop_limiter.global_position = body_position.global_position + Vector2(40, 0)
+	drop_limiter.global_position = body.global_position + Vector2(40, 0)
 	get_parent().call_deferred("add_child", drop_limiter)
 	
 	queue_free()
