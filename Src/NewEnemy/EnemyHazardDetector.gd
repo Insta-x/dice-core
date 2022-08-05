ceec906661b7c46c4e2a7527658f2750686c83d3
Insta-x <uttsadajason@outlook.com>
2022-08-05 15:25:27 +0700

    HackBullet collision

--- Src/NewEnemy/EnemyHazardDetector.gd
+++ Src/NewEnemy/EnemyHazardDetector.gd
@@ -1,13 +1,13 @@
 extends Area2D
 
 
-signal limit_bullet_detected(lower_limit, upper_limit)
+signal hack_bullet_detected(hack_seed)
 signal player_bullet_detected(roll)
 
 
 func _on_EnemyHazardDetector_body_entered(body: Bullet) -> void:
-	if body is LimitBullet:
-		emit_signal("limit_bullet_detected", body.lower_limit, body.upper_limit)
+	if body is HackBullet:
+		emit_signal("hack_bullet_detected", body.hack_seed)
 	else:
 		emit_signal("player_bullet_detected", body.roll)
 	
--- Src/Objects/HackBullet.gd
+++ Src/Objects/HackBullet.gd
@@ -1,5 +1,7 @@
 extends Bullet
 
+class_name HackBullet
+
 
 export (int) var hack_seed := 1
 
