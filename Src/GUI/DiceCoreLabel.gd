db0beed5387fe92f46d521a8fe5f85b8d77ff22c
Insta-x <uttsadajason@outlook.com>
2022-08-07 15:59:32 +0700

    Enemy dice seed UI init

--- Src/GUI/DiceCoreLabel.gd
+++ Src/GUI/DiceCoreLabel.gd
@@ -24,6 +24,10 @@ func show_true_seed() -> void:
 	text = str(true_seed)
 
 
+func _on_dice_seed_init(init_seed: int) -> void:
+	text = str(init_seed)
+
+
 func _on_dice_rolled(next_seed: int, limited_seed: int) -> void:
 	false_seed = next_seed
 	true_seed = limited_seed
