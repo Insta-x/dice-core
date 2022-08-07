883ab928e5682d9172f779421e14a62db1f49546
Insta-x <uttsadajason@outlook.com>
2022-08-07 15:39:22 +0700

    Delayed enemy dice roll

--- Src/GUI/DiceTimerProgress.gd
+++ Src/GUI/DiceTimerProgress.gd
@@ -2,10 +2,10 @@ extends TextureProgress
 
 
 export (NodePath) onready var timer = get_node(timer) as Timer
-export (float) var offset := 0.0
+export (float) var time_offset := 0.0
 
 
 func _process(delta: float) -> void:
 	if timer:
-		var temp : float = (timer.wait_time - timer.time_left) / timer.wait_time * max_value + offset
+		var temp : float = (timer.wait_time - timer.time_left + time_offset) / timer.wait_time * max_value
 		value = temp + max_value if temp < 0 else temp - max_value if temp > max_value else temp
