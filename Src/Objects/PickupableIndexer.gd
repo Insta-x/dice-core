1800a8dadbdaede8c618ea6536ae79afb63d6722
Insta-x <uttsadajason@outlook.com>
2022-08-27 13:10:59 +0700

    Pickupable limiter and indexer

--- Src/Objects/PickupableIndexer.gd
+++ Src/Objects/PickupableIndexer.gd
@@ -1,13 +1,13 @@
 extends Pickupable
 
-class_name PickupableLimiter
+class_name PickupableIndexer
 
 
-export (int) var limit := 2
+export (int) var start := 2
 
 
 func _ready() -> void:
-	$Sprite/Label.text = str(limit)
+	$Sprite/Label.text = str(start)
 	
 	yield(get_tree().create_timer(10), "timeout")
 	queue_free()
