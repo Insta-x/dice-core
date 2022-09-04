f4b4e32e4d04a042f58c2a9f5e93e00c9c4f8eb8
Insta-x <uttsadajason@outlook.com>
2022-09-04 13:43:22 +0700

    fix mouse input

--- Src/GUI/GameGUI/DiceSetSelection/DiceSetSelection.gd
+++ Src/GUI/GameGUI/DiceSetSelection/DiceSetSelection.gd
@@ -7,6 +7,6 @@ func _input(event: InputEvent) -> void:
 	
 	if event.is_action_pressed("ui_accept"):
 		GlobalSignals.emit_signal("dice_set_selected")
-		queue_free()
 		
 		get_tree().set_input_as_handled()
+		queue_free()
