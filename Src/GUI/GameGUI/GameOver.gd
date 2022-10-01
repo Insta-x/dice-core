4ca195e9a03c14008cce977210f7676931f71b08
Insta-x <uttsadajason@outlook.com>
2022-10-01 22:30:52 +0700

    game over redesign

--- Src/GUI/GameGUI/GameOver.gd
+++ Src/GUI/GameGUI/GameOver.gd
@@ -1,18 +1,27 @@
 extends NinePatchRect
 
 
+const tips := [
+	"Is it possible to manipulate your dice seed?",
+	
+]
+
+
 func _ready() -> void:
 	GlobalSignals.connect("player_died", self, "_on_game_over")
 	GlobalSignals.connect("time_over", self, "_on_game_over")
+	_on_game_over()
 
 
 func _input(event: InputEvent) -> void:
 	if visible:
 		if event.is_action_pressed("pause"):
 			get_tree().change_scene("res://Src/Game.tscn")
+		if event.is_action_pressed("restart_tutorial"):
+			get_tree().change_scene("res://Src/TestingArea.tscn")
 		get_tree().set_input_as_handled()
 
 
 func _on_game_over() -> void:
+	$VBoxContainer/TipsLabel.text = tips[randi() % tips.size()]
 	show()
-	$VBoxContainer/ScoreLabel.text = str(ScoreTracker.score)
