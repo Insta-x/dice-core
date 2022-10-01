278913b40e9f6bbfba8b9ffd855e7bc3448e922e
Insta-x <uttsadajason@outlook.com>
2022-10-01 22:44:07 +0700

    add tips

--- Src/GUI/GameGUI/GameOver.gd
+++ Src/GUI/GameGUI/GameOver.gd
@@ -3,14 +3,17 @@ extends NinePatchRect
 
 const tips := [
 	"Is it possible to manipulate your dice seed?",
-	
+	"I don't think it's possible to win without crits",
+	"Knowing how dice core generate dice seed would be cool",
+	"Don't forget... you have the hack gun",
 ]
 
 
 func _ready() -> void:
+#	randomize()
 	GlobalSignals.connect("player_died", self, "_on_game_over")
 	GlobalSignals.connect("time_over", self, "_on_game_over")
-	_on_game_over()
+#	_on_game_over()
 
 
 func _input(event: InputEvent) -> void:
