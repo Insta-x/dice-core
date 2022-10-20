42aaca7572e2c50540b069e20c716bbf82731f85
Insta-x <uttsadajason@outlook.com>
2022-10-20 21:02:10 +0700

    fix tutorial enemy gui hider and tutorial background

--- Src/NewEnemy/DummyEnemyWrapper.gd
+++
@@ -1,8 +0,0 @@
-extends EnemyWrapper
-
-
-func hacked(hack_seed: int) -> void:
-	.hacked(hack_seed)
-	
-	if hack_seed == 6:
-		GlobalSignals.emit_signal("tutorial_complete")
--- Src/NewEnemy/TutorialEnemyWrapper.gd
+++ Src/NewEnemy/TutorialEnemyWrapper.gd
@@ -1,6 +1,14 @@
 extends EnemyWrapper
 
 
-func dead() -> void:
-	.dead()
-	GlobalSignals.emit_signal("batu_died")
+func hacked(hack_seed: int) -> void:
+	.hacked(hack_seed)
+	
+	if hack_seed == 6:
+		GlobalSignals.emit_signal("tutorial_complete")
+
+
+func show_gui() -> void:
+	$GUIwrapper/EnemyGUI/VBoxContainer/HBoxContainer/MarginContainer/HealthContainer/ResultNumber.show()
+	$GUIwrapper/EnemyGUI/VBoxContainer/HBoxContainer/HBoxContainer.show()
+	$GUIwrapper/EnemyGUI/VBoxContainer/DiceTimerProgress.show()
